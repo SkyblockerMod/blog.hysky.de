4c99b292431100cc023b376ee4530419de417718
Yasin <19829407+LifeIsAParadox@users.noreply.github.com>
2025-05-01 02:27:21 +0200

    fix screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,21 @@ module.exports = {
       fontFamily: {
         sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        'landscape-touch': {
+          'raw': '(orientation: landscape) and (pointer: coarse)'
+        },
+        'sm-landscape-touch': {
+          raw: '(min-width: 640px) and (max-width: 767px) and (orientation: landscape) and (pointer: coarse)',
+        },
+        'md-landscape-touch': {
+          raw: '(min-width: 768px) and (max-width: 1023px) and (orientation: landscape) and (pointer: coarse)',
+        },
+        'lg-landscape-touch': {
+          raw: '(min-width: 1024px) and (orientation: landscape) and (pointer: coarse)',
+        },
+        ...defaultTheme.screens,
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
